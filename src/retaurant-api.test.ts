f7a33ca75a37330ef9af01a95dfd116dcbb01a90
JeonB <newjeonb@gmail.com>
2022-02-04 09:00:00 +0900

    Your commit message

--- src/retaurant-api.test.ts
+++ src/retaurant-api.test.ts
@@ -1,52 +1,110 @@
 import request from "supertest";
-// describe: 테스트 스위트, 관련 테스트 케이스들을 그룹화하여 실행하는 단위
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import fastify from "fastify";
+
+// 가짜 응답 데이터
+const fakeRestaurants = [
+  { id: 1, name: "음식점 A", location: "위치 A" },
+  { id: 2, name: "음식점 B", location: "위치 B" },
+];
+
+// Fastify 애플리케이션 생성
+const app: FastifyInstance = fastify();
+
+// GET /restaurants 라우트 정의
+app.get(
+  "/restaurants",
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    reply.code(200).send(fakeRestaurants);
+  }
+);
+
+// POST /restaurants 라우트 정의
+app.post(
+  "/restaurants",
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    const newRestaurant = request.body;
+    // 실제로 데이터베이스에 추가하는 것이 아니라 가짜 응답을 반환
+    reply.code(201).send(newRestaurant);
+  }
+);
+
+// PUT /restaurants/:id 라우트 정의
+app.put(
+  "/restaurants/:id",
+  async (
+    request: FastifyRequest<{ Params: { id: string } }>,
+    reply: FastifyReply
+  ) => {
+    const id = request.params.id;
+    const updatedRestaurant = request.body;
+    // 실제로 데이터베이스를 수정하는 것이 아니라 가짜 응답을 반환
+    reply.code(200).send(updatedRestaurant);
+  }
+);
+
+// DELETE /restaurants/:id 라우트 정의
+app.delete(
+  "/restaurants/:id",
+  async (
+    request: FastifyRequest<{ Params: { id: string } }>,
+    reply: FastifyReply
+  ) => {
+    const id = request.params.id;
+    // 실제로 데이터베이스에서 삭제하는 것이 아니라 가짜 응답을 반환
+    reply.code(204).send();
+  }
+);
+
+// 테스트 코드
 describe("음식점 데이터를 다루는 REST API", () => {
   it("GET /restaurants - 음식점 목록을 가져올 수 있어야 함", async () => {
-    // Given
-    // When
+    // When: /restaurants 에 GET 요청을 보냈을 때
     const response = await request(app).get("/restaurants");
 
-    // Then
+    // Then: 응답 상태코드는 200이어야 하고, 응답 바디는 가짜 음식점 목록이어야 함
     expect(response.status).toBe(200);
-    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body).toEqual(fakeRestaurants);
   });
 
   it("POST /restaurants - 음식점을 추가할 수 있어야 함", async () => {
-    // Given
+    // Given: 새로운 음식점 데이터
     const newRestaurant = { name: "새로운 음식점", location: "위치" };
 
-    // When
+    // When: /restaurants 에 POST 요청을 보냈을 때
     const response = await request(app)
       .post("/restaurants")
       .send(newRestaurant);
 
-    // Then
+    // Then: 응답 상태코드는 201이어야 하고, 응답 바디는 새로운 음식점 데이터와 일치해야 함
     expect(response.status).toBe(201);
     expect(response.body).toMatchObject(newRestaurant);
   });
 
   it("PUT /restaurants/:id - 음식점 정보를 수정할 수 있어야 함", async () => {
-    // Given
+    // Given: 수정된 음식점 데이터와 음식점 ID
     const updatedRestaurant = {
       name: "수정된 음식점",
       location: "새로운 위치",
     };
+    const restaurantId = 1; // 가짜 음식점의 ID로 가정
 
-    // When
+    // When: /restaurants/:id 에 PUT 요청을 보냈을 때
     const response = await request(app)
-      .put("/restaurants/1") // 존재하는 음식점의 ID로 가정합니다.
+      .put(`/restaurants/${restaurantId}`)
       .send(updatedRestaurant);
 
-    // Then
+    // Then: 응답 상태코드는 200이어야 하고, 응답 바디는 수정된 음식점 데이터와 일치해야 함
     expect(response.status).toBe(200);
     expect(response.body).toMatchObject(updatedRestaurant);
   });
 
   it("DELETE /restaurants/:id - 음식점을 삭제할 수 있어야 함", async () => {
-    // Given, when
-    const response = await request(app).delete("/restaurants/1"); // 존재하는 음식점의 ID로 가정합니다.
+    // Given, When: /restaurants/:id 에 DELETE 요청을 보냈을 때
+    const restaurantId = 1; // 가짜 음식점의 ID로 가정
+    const response = await request(app).delete(`/restaurants/${restaurantId}`);
 
-    // Then
+    // Then: 응답 상태코드는 204여야 함
     expect(response.status).toBe(204);
   });
 });
